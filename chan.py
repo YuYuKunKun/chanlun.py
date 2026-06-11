3f70e618cf65ccf80a71e33275a6d4a11734c225
YuWuKunCheng <youwukuncheng@163.com>
2026-06-11 10:47:42 +0800

    添加 全局变量“扩展线段模式” 默认为 False 线段 扩展分析采用“端点高, 端点低” 作为高低取值。中枢分析时高低取值不变，此举在中枢<扩展**> 中更贴合原文同级别分级时的中枢高低取值

--- chan.py
+++ chan.py
@@ -3130,6 +3130,9 @@ class 分型:
         分型序列.append(当前分型)
 
 
+扩展线段模式 = False  # TODO 虚线高低取值 暂定，此举将符合同级别分解时正确的高低取值涉及中枢等问题
+
+
 class 虚线:
     """笔/线段的通用数据结构，持有一组分型端点（文=起点分型, 武=终点分型）。
 
@@ -3258,14 +3261,28 @@ class 虚线:
                 raise RuntimeError("无法识别的方向", self.文.结构, self.武.结构)
 
     @property
+    def 端点高(self) -> float:
+        if self.方向 is 相对方向.向上:
+            return self.武.中.高
+        return self.文.中.高
+
+    @property
+    def 端点低(self) -> float:
+        if self.方向 is 相对方向.向下:
+            return self.武.中.低
+        return self.文.中.低
+
+    @property
     def 高(self) -> float:
         """虚线区间的最高价。
 
         :return: 向上虚线取武.中.高，向下虚线取文.中.高
         """
-        if self.方向 is 相对方向.向上:
-            return self.武.中.高
-        return self.文.中.高
+        if 扩展线段模式 and self.模式 != "文武" and self.标识 != "笔" and "扩展" in self.标识:  # 扩展线段
+            端点序列 = [筆.文 for 筆 in self.基础序列]
+            端点序列.append(self.基础序列[-1].武)
+            return max(端点序列, key=lambda o: o.中.高).中.高
+        return self.端点高
 
     @property
     def 低(self) -> float:
@@ -3273,9 +3290,11 @@ class 虚线:
 
         :return: 向下虚线取武.中.低，向上虚线取文.中.低
         """
-        if self.方向 is 相对方向.向下:
-            return self.武.中.低
-        return self.文.中.低
+        if 扩展线段模式 and self.模式 != "文武" and self.标识 != "笔" and "扩展" in self.标识:  # 扩展线段
+            端点序列 = [筆.文 for 筆 in self.基础序列]
+            端点序列.append(self.基础序列[-1].武)
+            return min(端点序列, key=lambda o: o.中.低).中.低
+        return self.端点低
 
     def 之前是(self, 之前: 虚线) -> bool:
         """
@@ -5268,7 +5287,7 @@ class 线段:
         if not 线段序列:
             for i in range(1, len(虚线序列) - 1):
                 左, 中, 右 = 虚线序列[i - 1], 虚线序列[i], 虚线序列[i + 1]
-                关系 = 相对方向.分析(左.高, 左.低, 右.高, 右.低)
+                关系 = 相对方向.分析(左.端点高, 左.端点低, 右.端点高, 右.端点低)
                 if 关系 not in (相对方向.向下, 相对方向.向上, 相对方向.顺, 相对方向.逆, 相对方向.同):  # FIXME 此处为首个线段
                     continue
 
@@ -5288,7 +5307,7 @@ class 线段:
 
         if not 配置.扩展线段_当下分析:
             左, 中, 右 = 当前线段.基础序列[:3]
-            if not 相对方向.分析(左.高, 左.低, 右.高, 右.低).是否缺口():
+            if not 相对方向.分析(左.端点高, 左.端点低, 右.端点高, 右.端点低).是否缺口():
                 当前线段.基础序列[:] = 当前线段.基础序列[:3]
                 线段._武终(当前线段, sys._getframe().f_lineno)
             else:
@@ -5305,7 +5324,7 @@ class 线段:
 
         for i in range(序号 + 1, len(虚线序列) - 1):
             左, 中, 右 = 虚线序列[i - 1], 虚线序列[i], 虚线序列[i + 1]
-            相对关系 = 相对方向.分析(左.高, 左.低, 右.高, 右.低)
+            相对关系 = 相对方向.分析(左.端点高, 左.端点低, 右.端点高, 右.端点低)
             if 相对关系.是否缺口():
                 线段._添加虚线(当前线段, 左)
                 线段._添加虚线(当前线段, 中)
@@ -6205,7 +6224,7 @@ class 观察者:
             传入差异 = 缠论配置().对比(配置)
             传入差异.update(差异)
             配置 = 缠论配置(**传入差异)
-            print("加载异常配置+传入差异", 传入差异)
+            logger.info(f"加载异常配置+传入差异: {传入差异}")
 
         name = Path(文件路径).name.split(".")[0]
         符号, 周期, 起始时间戳, 结束时间戳 = name.split("-")
